ad84dc4cefc6a313f5bbcd7c61c60008db9b9efe
Kaustubh Mahamuni <91269914+Kaustubh1708@users.noreply.github.com>
2022-05-16 23:10:21 +0530

    Update SCOB61_Assignment1.cpp

--- SCOB61_Assignment1.cpp
+++ SCOB61_Assignment1.cpp
@@ -1,100 +1,123 @@
-/* ASSIGNMENT NO.: 1 ; Roll No.:-SCOB54
+//Assignment No. 1 
+/* Problem Statement: A book consists of chapters, chapters consist of 
+sections and sections consist of 
+subsections. Construct a tree and print the nodes. Find the time and space 
+requirements 
+of your method. */ 
+#include <iostream> #include <string.h> 
+using namespace std; 
 
-Title :A book consists of chapters, chapters consist of sections and sections consist of subsections. 
-Construct a tree and print the nodes. Find the time and space requirements of your method.  */
+struct node // Node Declaration 
+{
+char label[10]; 
+int ch_count; 
+struct node *child[10]; 
+}*root; 
 
-#include<iostream>
-#include<string.h>
-using namespace std;
-// DECLARATION OF NODE
-struct node
+class Book // Class Declaration 
 {
-char label[20];                 // To store the name of chapter and section
-int chcount;                     // To store the no of childs
-node *child[50];             // Array of pointer to store addresses of node
-}*root;
-// DECLARATION OF CLASS
-class BOOK
+public: 
+
+Book() 
 {
-public:             // FUNCTION DECLARATION
-void insert();            
-void display();
-BOOK()              // CONSTRUCTOR
-{
-root == NULL;
+root = NULL; 
 }
-};
-void BOOK::insert()
+
+
+void create() 
 {
-int secount;
-root = new node();                                                // THE BOOK IS  CREATED
-cout<<"Enter the name of book:"<<endl;
-cin>>root->label;               // Arrow operator is used for accessing elements in the structure using pointer
-cout<<"Enter the total number of chapters in book:"<<endl;
-cin>>root->chcount;
-for(int i=0;i<root->chcount;i++)
+int tbooks, tchapters, tsections, i, j, k; 
+
+root = new node; 
+cout << "Enter head of book : "; 
+cin>>root->label; 
+
+cout << "Enter number of chapters in book : "; 
+cin >> tchapters; 
+root->ch_count = tchapters; 
+for (i = 0; i < tchapters; i++) 
 {
-root->child[i] = new node();                               // CHAPTERS IS CREATED    
-cout<<"Enter the name of chapters:"<<endl;
-cin>>root->child[i]->label;
-cout<<"Enter the number of sections:"<<endl;
-cin>>root->child[i]->chcount;
-for(int j=0;j<root->child[i]->chcount;j++)
+root->child[i] = new node; 
+cout << "Enter the name of Chapter " << i + 1 << " : "; 
+cin>>root->child[i]->label; 
+
+cout << "Enter number of sections in Chapter " << root- 
+>child[i]->label << " : "; 
+cin >> tsections; 
+root->child[i]->ch_count = tsections; 
+
+for (j = 0; j < tsections; j++) 
 {
-root->child[i]->child[j] = new node();             // SECTIONS ARE CREATED
-cout<<"Enter the name of section:"<<endl;
-cin>>root->child[i]->child[j]->label;
-cout<<"Enter the number of sub sections:"<<endl;
-cin>>root->child[i]->child[j]->chcount;
-for(int k=0; k<root->child[i]->child[j]->chcount; k++)
-{
-root->child[i]->child[j]->child[k] = new node();          // SUBSECTIONS ARE CREATED
-cout<<"Enter the name of sub section:"<<endl;
-cin>>root->child[i]->child[j]->child[k]->label;
+root->child[i]->child[j] = new node; 
+cout << "Enter the name of section " << j+1 << " : "; 
+cin>>root->child[i]->child[j]->label; 
+ 
 }
 }
 }
-}
-void BOOK::display()
+
+void display(node *r1) 
 {
-if(root != NULL)
+cout<<"\n\n******************"<<r1- 
+>label<<"******************"<<endl; 
+
+for(int i=0; i<r1->ch_count;i++) 
 {
-cout<<"** Hierarchy of Book **"<<endl;
-cout<<"Book Name is"<<root->label<<endl;
-for(int i=0; i<root->chcount; i++)
+cout<<"\n"<<i+1<<"\t"<<r1->child[i]->label; 
+for (int j=0; j<r1->child[i]->ch_count; j++) 
 {
-cout<<"Chapter name is  "<<root->child[i]->label<<endl;
-for(int j=0; j<root->child[i]->chcount; j++)
+cout<<"\n\t"<<j+1<<"\t"<<r1->child[i]->child[j]->label; 
+}
+cout<<"\n---------------------------------------------------"; 
+}
+
+}
+
+
+}; 
+
+
+int main() 
 {
-cout<<"Section name is "<<root->child[i]->child[j]->label<<endl;
-for(int k=0; k<root->child[i]->child[j]->chcount; k++)
-{
-cout<<"Sub-section name is "<<root->child[i]->child[j]->child[k]->label<<endl;
+Book b1; 
+
+b1.create(); 
+b1.display(root); 
+
+return 0; 
 }
-}
-}
-}
-}
-int main()
-{
-BOOK g;
-int ch;
-do
-{
-cout<<"** MENU****"<<endl;
-cout<<"1. Insert."<<endl;
-cout<<"2. Display."<<endl;
-cout<<"3. EXIT."<<endl;
-cout<<"Enter the choice:"<<endl;
-cin>>ch;
-switch(ch)
-{
-case 1: g.insert();
-break;
-case 2: g.display();
-break;
-case 3:
-            exit(1);
-}
-}while(ch<4);
-}
+
+/*Output: 
+Enter head of book : DSA 
+ 
+Enter number of chapters in Enter the name of Chapter 1 Enter number of sections in Enter the name of section 1 Enter the name of section 2 Enter the name of section 3 Enter the name of Chapter 2 Enter number of sections in Enter the name of section 1 Enter the name of section 2 Enter the name of Chapter 3 Enter number of sections in Enter the name of section 1 Enter the name of section 2 
+ 
+book : 3 
+: Graph 
+Chapter Graph : 3 : Adjacencymatrix 
+: DFS : BFS 
+: Tree 
+Chapter Tree : 2 
+: General 
+: Binary 
+: Hashing 
+Chapter Hashing : 2 
+: Probing 
+: Chaining 
+ 
+
+
+******************DSA****************** 
+ 
+1. Graph 
+1. Adjacencymatrix 
+2. DFS 3. BFS 
+--------------------------------------------------- 
+4. Tree 
+1. General 
+2. Binary 
+--------------------------------------------------- 
+3. Hashing 
+1. Probing 
+2. Chaining 
+--------------------------------------------------- */
